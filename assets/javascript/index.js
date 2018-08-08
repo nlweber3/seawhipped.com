c86fc2e8d4239d410acedabbe411346f29dd9a8d
Nick <nlweber3@gmail.com>
2018-08-08 15:56:12 -0700

    changes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,21 +1,8 @@
 //parallax image on scroll
 $(document).ready(function () {
   $('.parallax').parallax();
-});
-
-
 //initialize sidenav for mobile sizing
-$(document).ready(function () {
   $('.sidenav').sidenav();
-});
-
-//carousel on images
-$(document).ready(function () {
-  $('.carousel').carousel();
-});
-
 //zoom on image on click
-$(document).ready(function(){
   $('.materialboxed').materialbox();
 });
-    
